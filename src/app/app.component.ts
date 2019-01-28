db8494ab5d08da5a7560a645089913b9b220dc62
Jusoh <jujusoh@apac.corpdir.net>
2019-01-28 15:46:58 +0800

    2019-01-28 15:46

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from './git-search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GitSearchService]
 })
 export class AppComponent {
-  title = 'angular-fundamentals';
+  constructor(private GitSearchService: GitSearchService) {
+
+  }
+  ngOnInit() {
+    this.GitSearchService.gitSearch('angular').then( (response) => {
+      alert("Total Libraries Found:" + response.total_count);
+    }, (error) => {
+      alert("Error: " + error.statusText)
+    })
+  }
+  //title = 'Github Browser';
+  title = 'app is functional!';
 }
---
+++ src/app/git-search.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { GitSearch } from './git-search'
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GitSearchService {
+  cachedValues: Array<{
+    [query: string]: GitSearch
+  }> = [];
+  constructor(http: HttpClient) {
+    //this.http = http
+  }
+  gitSearch = (query: string): Promise<GitSearch> => {
+    let promise = new Promise((resolve, reject) => {
+      if (this.cachedValues[query]) {
+        resolve(this.cachedValues[query])
+      } else {
+        //resolve("Placeholder");
+        this.http.get('https://api.github.com/search/repositories?q=' + query).toPromise().then((response)=>{
+          resolve(response)
+        }, (error) => {
+          reject(error);
+        })
+      }
+    })
+    return promise;
+  }
+}
